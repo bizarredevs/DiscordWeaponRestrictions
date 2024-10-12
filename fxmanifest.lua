d3f7f5a92bf08c9bd9b905708e04b187f4135386
Bizarre <89548876+bizarredevs@users.noreply.github.com>
2024-10-12 16:44:52 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Bizarre'
+description 'Discord Role Weapon Restriction Script for FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
+
+dependencies {
+    'DiscordRoles' -- Ensure that you have the DiscordRoles resource installed
+}
